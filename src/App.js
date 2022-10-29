3435f62a28f61789e4fc8c5e56e8142323f0f8c2
KD <omroners@gmail.com>
2022-10-29 17:06:10 +0300

    modal app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    const [open, setOpen] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="modal">
+        <p className="message">Открой меня</p>
+        <div className="options">
+          <button className="btn" onClick={() => setOpen(true)}>Модальное окно</button>
+        </div>
+      </div>
+        {/*ХОРОШО КОГДА НЕТУ АНИМАЦИИ*/}
+        {open && (
+            <div className="overlay" onClick={() => setOpen(false)}>
+            <div className="overlay-in">
+                <div className="overlay-modal">
+                    <iframe src="https://giphy.com/embed/5DQdk5oZzNgGc" width="480" height="403" frameBorder="0"
+                            className="giphy-embed" allowFullScreen></iframe>
+                    <div className="options-overlay">
+                        <button className="btn" onClick={() => setOpen(false)}>Закрыть окно</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        )}
+
     </div>
   );
 }
